0c83cb5511943aaf3e1d1f4b8dc54e22616d78bf
Daniel Ferreira <daniel.ferreira.dev@gmail.com>
2023-11-25 18:15:53 -0300

    first commit

---
+++ src/m2/v2_Boolean/before/PrimitiveBooleans.java
@@ -0,0 +1,12 @@
+package m2.v2_Boolean.before;
+import static java.lang.System.out;
+
+public class PrimitiveBooleans {
+    public static void main(String[] args) {
+        out.println("\n** Primitive booleans ** \n");
+        boolean flag = true;
+        flag = false;
+
+        out.println(flag);
+    }
+}
